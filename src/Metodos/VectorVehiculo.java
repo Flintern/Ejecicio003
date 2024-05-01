ffca71f11ef0af9eeb46904ccc28056a500ee19b
jesus <jesus@PortatilJesus>
2024-05-01 18:28:04 -0500

    importe las librerias de swing y agregue los atributos y el metodo VectorVehiculo

--- src/Metodos/VectorVehiculo.java
+++ src/Metodos/VectorVehiculo.java
@@ -4,10 +4,40 @@
  */
 package Metodos;
 
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 /**
  *
  * @author jesus
  */
 public class VectorVehiculo {
+    
+    Vehiculo miVehiculo[];
+    int tam;
+    
+    VectorVehiculo(JTextField CajaTam){
+           int i;
+        try{
+            
+            tam = Integer.parseInt(CajaTam.getText());
+            //Declarar el vector
+            miVehiculo = new Vehiculo[tam];
+            //Instanciar el vector por cada posición de forma
+            //vacía
+            for( i=0; i<miVehiculo.length; i++ ){
+                miVehiculo[i] = new Vehiculo("", "", "", 0,false);
+            }
+        }catch(Exception e){
+            JOptionPane.showMessageDialog(null,
+                "Error: "+e+".  El vector no fue creado! Intente nuevamente.");
+            CajaTam.setText("");
+            CajaTam.requestFocus();
+            miVehiculo = null;
+        }
+    }
+    
+    
     
 }
