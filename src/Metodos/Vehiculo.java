16e1052586d6e789b7c9854462a52c98b11510be
jesus <jesus@PortatilJesus>
2024-05-01 00:31:46 -0500

    agregado constructor y metodo getMostrarVehiculo, quite los private de los atributos

--- src/Metodos/Vehiculo.java
+++ src/Metodos/Vehiculo.java
@@ -4,16 +4,40 @@
  */
 package Metodos;
 
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author jesus
  */
 public class Vehiculo {
-    
-    private String matricula;
-    private String marca;
-    private String modelo;
-    private double precio;
-    private boolean estado;
-    
+
+    String matricula;
+    String marca;
+    String modelo;
+    double precio;
+    boolean estado;
+
+    public Vehiculo(String matricula, String marca, String modelo, double precio, boolean estado) {
+        this.matricula = matricula;
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.estado = estado;
+    }
+
+    public void getMostrarVehiculo() {
+        String info = "Los datos de el vehiculo son: \n";
+        info += "Matricula: " + matricula + "\n";
+        info += "Marca: " + marca + "\n";
+        info += "Modelo: " + modelo + "\n";
+        info += "Precio: " + precio + "\n";
+        if (estado == true) {
+            info += "Estado: Vendido\n";
+        } else {
+            info += "Estado: Disponible para venta\n";
+        }
+        JOptionPane.showMessageDialog(null, info);
+
+    }
 }
